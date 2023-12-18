f92e19aa30ca14f2e3a93a120c0161ad669e87cd
HondaChan14 <92832561+HondaChan14@users.noreply.github.com>
2023-12-18 17:06:38 -0500

    feat(skills): add skill category titles to skills component

--- src/components/SoftwareSkills/Skills.js
+++ src/components/SoftwareSkills/Skills.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Skills = ({ skills }) => {
+const Skills = ({ skills, title }) => {
 
   return (
     <>
+    <h4>{title}</h4>
       <ul className="dev-icons">
         {skills.map((skill) => (
           <li key={skill.skillName}>
--- src/components/SoftwareSkills/SoftwareSkills.js
+++ src/components/SoftwareSkills/SoftwareSkills.js
@@ -4,13 +4,14 @@ import Skills from './Skills';
 import { frontend, backend, devops, apidev, hosting } from '../../data/skillSection';
 
 const skillCategories = [ frontend, backend, devops, apidev, hosting ]
+const langTitles = ["Frontend", "Backend", "Dev Ops", "API Development", "Hosting"]
 
 export default function SoftwareSkills() {
   return (
     <div className="main-container">
       <div className="container column">
         {skillCategories && skillCategories.map((category, index) => (
-          <Skills key={index} skills={category} />
+          <Skills key={index} skills={category} title={langTitles[index]}/>
         ))}
       </div>
     </div>
